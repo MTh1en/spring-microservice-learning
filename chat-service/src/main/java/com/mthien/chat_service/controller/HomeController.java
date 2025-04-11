e7386ff14546d7b9ddcf9be960f8e555ce8306a1
MTh1en <mt12122003@gmail.com>
2025-04-11 16:40:35 +0700

    add chat-service

---
+++ chat-service/src/main/java/com/mthien/chat_service/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.mthien.chat_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/group-chat")
+    public ModelAndView index() {
+        return new ModelAndView("group-chat");
+    }
+
+    @GetMapping("/home")
+    public String honme() {
+        return "home";
+    }
+}
